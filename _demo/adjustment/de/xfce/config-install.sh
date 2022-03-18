1658bbc4ec83aff92c23231a758a946e09aa21d8
samwhelp <samwhelp@users.noreply.github.com>
2022-03-18 21:20:59 +0800

    thunar

--- _demo/adjustment/de/xfce/config-install.sh
+++ _demo/adjustment/de/xfce/config-install.sh
@@ -39,8 +39,8 @@ xfce4_config_install () {
 	install -Dm644 "./config/xfce4/panel/whiskermenu-1.rc" "$HOME/.config/xfce4/panel/whiskermenu-1.rc"
 
 
-	echo "install -Dm644 ./config/xfce4/helpers.rc $HOME/.config/xfce4/helpers.rc"
-	install -Dm644 "./config/xfce4/helpers.rc" "$HOME/.config/xfce4/helpers.rc"
+	#echo "install -Dm644 ./config/xfce4/helpers.rc $HOME/.config/xfce4/helpers.rc"
+	#install -Dm644 "./config/xfce4/helpers.rc" "$HOME/.config/xfce4/helpers.rc"
 
 
 	echo
@@ -129,8 +129,23 @@ thunar_config_install () {
 	mkdir -p "$HOME/.config/Thunar"
 
 
-	echo "install -Dm644 ./config/thunar/uca.xml $HOME/.config/Thunar/uca.xml"
-	install -Dm644 "./config/thunar/uca.xml" "$HOME/.config/Thunar/uca.xml"
+	echo "install -Dm644 ./config/thunar/Thunar/uca.xml $HOME/.config/Thunar/uca.xml"
+	install -Dm644 "./config/thunar/Thunar/uca.xml" "$HOME/.config/Thunar/uca.xml"
+
+	echo "install -Dm644 ./config/thunar/Thunar/accels.scm $HOME/.config/Thunar/accels.scm"
+	install -Dm644 "./config/thunar/Thunar/accels.scm" "$HOME/.config/Thunar/accels.scm"
+
+	echo
+
+	echo "mkdir -p $HOME/.config/xfce4/xfconf/xfce-perchannel-xml"
+	mkdir -p "$HOME/.config/xfce4/xfconf/xfce-perchannel-xml"
+
+	echo "install -Dm644 ./config/thunar/xfce4/xfconf/xfce-perchannel-xml/thunar.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml"
+	install -Dm644 "./config/thunar/xfce4/xfconf/xfce-perchannel-xml/thunar.xml" "$HOME/.config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml"
+
+	echo "install -Dm644 ./config/thunar/xfce4/helpers.rc $HOME/.config/xfce4/helpers.rc"
+	install -Dm644 "./config/thunar/xfce4/helpers.rc" "$HOME/.config/xfce4/helpers.rc"
+
 
 	echo
 }
--- _demo/adjustment/tool/thunar/config-install.sh
+++ _demo/adjustment/tool/thunar/config-install.sh
@@ -15,22 +15,22 @@ thunar_config_install () {
 	mkdir -p "$HOME/.config/Thunar"
 
 
-	echo "install -Dm644 ./config/Thunar/uca.xml $HOME/.config/Thunar/uca.xml"
-	install -Dm644 "./config/Thunar/uca.xml" "$HOME/.config/Thunar/uca.xml"
+	echo "install -Dm644 ./config/thunar/Thunar/uca.xml $HOME/.config/Thunar/uca.xml"
+	install -Dm644 "./config/thunar/Thunar/uca.xml" "$HOME/.config/Thunar/uca.xml"
 
-	echo "install -Dm644 ./config/Thunar/accels.scm $HOME/.config/Thunar/accels.scm"
-	install -Dm644 "./config/Thunar/accels.scm" "$HOME/.config/Thunar/accels.scm"
+	echo "install -Dm644 ./config/thunar/Thunar/accels.scm $HOME/.config/Thunar/accels.scm"
+	install -Dm644 "./config/thunar/Thunar/accels.scm" "$HOME/.config/Thunar/accels.scm"
 
 	echo
 
 	echo "mkdir -p $HOME/.config/xfce4/xfconf/xfce-perchannel-xml"
 	mkdir -p "$HOME/.config/xfce4/xfconf/xfce-perchannel-xml"
 
-	echo "install -Dm644 ./config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml"
-	install -Dm644 "./config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml" "$HOME/.config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml"
+	echo "install -Dm644 ./config/thunar/xfce4/xfconf/xfce-perchannel-xml/thunar.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml"
+	install -Dm644 "./config/thunar/xfce4/xfconf/xfce-perchannel-xml/thunar.xml" "$HOME/.config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml"
 
-	echo "install -Dm644 ./config/xfce4/helpers.rc $HOME/.config/xfce4/helpers.rc"
-	install -Dm644 "./config/xfce4/helpers.rc" "$HOME/.config/xfce4/helpers.rc"
+	echo "install -Dm644 ./config/thunar/xfce4/helpers.rc $HOME/.config/xfce4/helpers.rc"
+	install -Dm644 "./config/thunar/xfce4/helpers.rc" "$HOME/.config/xfce4/helpers.rc"
 
 
 	echo
