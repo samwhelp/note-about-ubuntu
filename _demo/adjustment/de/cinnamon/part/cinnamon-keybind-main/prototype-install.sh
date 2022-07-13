a5f1711f143c254948eef5768c482fb40d4888fb
samwhelp <samwhelp@users.noreply.github.com>
2022-07-13 09:01:25 +0800

    fix

---
+++ _demo/adjustment/de/cinnamon/part/cinnamon-keybind-main/prototype-install.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+################################################################################
+### Head: cinnamon
+##
+cinnamon_prototype_install () {
+
+	##
+	## /usr/share/glib-2.0/schemas/50_cinnamon-keybind-main.gschema.override
+	##
+
+	##
+	## dconf dump /org/cinnamon/desktop/keybindings/wm/
+	##
+
+	echo
+	echo "##"
+	echo "## Prototype: cinnamon"
+	echo "##"
+	echo
+
+	echo "sudo mkdir -p /usr/share/glib-2.0/schemas"
+	sudo mkdir -p "/usr/share/glib-2.0/schemas"
+
+	echo "sudo install -Dm644 ./config/cinnamon/schemas/50_cinnamon-keybind-main.gschema.override /usr/share/glib-2.0/schemas/50_cinnamon-keybind-main.gschema.override"
+	sudo install -Dm644 "./config/cinnamon/schemas/50_cinnamon-keybind-main.gschema.override" "/usr/share/glib-2.0/schemas/50_cinnamon-keybind-main.gschema.override"
+
+	echo "sudo glib-compile-schemas /usr/share/glib-2.0/schemas/"
+	sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+	echo
+}
+##
+### Tail: cinnamon
+################################################################################
+
+
+################################################################################
+### Head: main
+##
+main_prototype_install () {
+	cinnamon_prototype_install
+}
+## start
+main_prototype_install
+
+##
+### Tail: main
+################################################################################
