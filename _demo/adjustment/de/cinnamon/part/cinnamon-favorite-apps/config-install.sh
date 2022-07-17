13a8311f80926f066b7091e834b02cf2afb97689
samwhelp <samwhelp@users.noreply.github.com>
2022-07-17 20:33:08 +0800

    fix

--- _demo/adjustment/de/cinnamon/part/cinnamon-favorite-apps/config-install.sh
+++ _demo/adjustment/de/cinnamon/part/cinnamon-favorite-apps/config-install.sh
@@ -35,6 +35,10 @@ cinnamon_favorite_apps_config () {
 	echo 'gsettings set org.cinnamon favorite-apps "'"['pcmanfm-qt.desktop', 'org.xfce.mousepad.desktop', 'org.kde.falkon.desktop', 'sakura.desktop', 'cinnamon-settings.desktop']"'"'
 	gsettings set org.cinnamon favorite-apps "['pcmanfm-qt.desktop', 'org.xfce.mousepad.desktop', 'org.kde.falkon.desktop', 'sakura.desktop', 'cinnamon-settings.desktop']"
 
+
+	echo "gsettings set org.cinnamon.desktop.default-applications.terminal exec 'sakura'"
+	gsettings set org.cinnamon.desktop.default-applications.terminal exec 'sakura'
+
 }
 
 ##
---
+++ _demo/adjustment/de/cinnamon/part/cinnamon-favorite-apps/prototype-install.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+################################################################################
+### Head: cinnamon
+##
+cinnamon_prototype_install () {
+
+	##
+	## /usr/share/glib-2.0/schemas/50_cinnamon-favorite-apps.gschema.override
+	##
+
+
+	echo
+	echo "##"
+	echo "## Prototype: cinnamon"
+	echo "##"
+	echo
+
+	echo "sudo mkdir -p /usr/share/glib-2.0/schemas"
+	sudo mkdir -p "/usr/share/glib-2.0/schemas"
+
+	echo "sudo install -Dm644 ./config/cinnamon/schemas/50_cinnamon-favorite-apps.gschema.override /usr/share/glib-2.0/schemas/50_cinnamon-favorite-apps.gschema.override"
+	sudo install -Dm644 "./config/cinnamon/schemas/50_cinnamon-favorite-apps.gschema.override" "/usr/share/glib-2.0/schemas/50_cinnamon-favorite-apps.gschema.override"
+
+	echo "sudo glib-compile-schemas /usr/share/glib-2.0/schemas/"
+	sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+	echo
+}
+##
+### Tail: cinnamon
+################################################################################
+
+
+################################################################################
+### Head: main
+##
+main_prototype_install () {
+	cinnamon_prototype_install
+}
+## start
+main_prototype_install
+
+##
+### Tail: main
+################################################################################
