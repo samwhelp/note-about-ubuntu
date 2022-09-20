a0239fd76e9e98d6321b1ab50208c9a7df556981
samwhelp <samwhelp@users.noreply.github.com>
2022-09-20 18:11:15 +0800

    fix

--- _legacy/22.10/adjustment/de/gnome-shell/part/gnome-shell-extension-ubuntu-dock/config-install.sh
+++ _legacy/22.10/adjustment/de/gnome-shell/part/gnome-shell-extension-ubuntu-dock/config-install.sh
@@ -9,15 +9,15 @@ set -e
 
 gnome_shell_favorite_apps_config () {
 
-	#gnome_shell_favorite_apps_config_contain_firefox
-	gnome_shell_favorite_apps_config_contain_falkon
+	gnome_shell_favorite_apps_config_contain_firefox
+	#gnome_shell_favorite_apps_config_contain_falkon
 
 }
 
 gnome_shell_favorite_apps_config_contain_firefox () {
 
 	echo 'gsettings set org.gnome.shell favorite-apps "'"['pcmanfm-qt.desktop', 'atom.desktop', 'firefox_firefox.desktop', 'sakura.desktop', 'gnome-control-center.desktop']"'"'
-	gsettings set org.gnome.shell favorite-apps "['pcmanfm-qt.desktop', 'atom.desktop', 'firefox_firefox.desktop', 'sakura.desktop', 'gnome-control-center.desktop', 'toggle-show-desktop.xdotool.desktop']"
+	gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'atom.desktop', 'firefox_firefox.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Settings.desktop', 'toggle-show-desktop.xdotool.desktop']"
 
 }
 
@@ -25,9 +25,10 @@ gnome_shell_favorite_apps_config_contain_firefox () {
 gnome_shell_favorite_apps_config_contain_falkon () {
 
 	echo 'gsettings set org.gnome.shell favorite-apps "'"['pcmanfm-qt.desktop', 'atom.desktop', 'org.kde.falkon.desktop', 'sakura.desktop', 'gnome-control-center.desktop']"'"'
-	gsettings set org.gnome.shell favorite-apps "['pcmanfm-qt.desktop', 'atom.desktop', 'org.kde.falkon.desktop', 'sakura.desktop', 'gnome-control-center.desktop', 'toggle-show-desktop.xdotool.desktop']"
+	gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'atom.desktop', 'org.kde.falkon.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Settings.desktop', 'toggle-show-desktop.xdotool.desktop']"
 
 }
+
 
 ##
 ### Tail: gnome-shell / favorite-apps
