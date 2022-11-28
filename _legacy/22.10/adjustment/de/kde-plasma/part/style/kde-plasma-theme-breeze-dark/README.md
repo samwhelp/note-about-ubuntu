bc310180da9d40b0b7083fb317489cdc21ef538f
samwhelp <samwhelp@users.noreply.github.com>
2022-11-28 18:27:26 +0800

    fix

---
+++ _legacy/22.10/adjustment/de/kde-plasma/part/style/kde-plasma-theme-breeze-dark/README.md
@@ -0,0 +1,59 @@
+
+# kde-plasma / Adjustment
+
+
+## Note
+
+run
+
+``` sh
+plasmashell --version
+```
+
+show
+
+```
+plasmashell 5.25.5
+```
+
+
+## Howto
+
+
+### install
+
+``` sh
+$ ./install.sh
+```
+
+or
+
+``` sh
+$ make install
+```
+
+
+### package-install
+
+``` sh
+$ ./package-install.sh
+```
+
+or
+
+``` sh
+$ make package-install
+```
+
+
+### config-install
+
+``` sh
+$ ./config-install.sh
+```
+
+or
+
+``` sh
+$ make config-install
+```
---
+++ _legacy/22.10/adjustment/de/kde-plasma/part/style/kde-plasma-theme-breeze-dark/package-install.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+################################################################################
+### Head: util_package
+##
+util_package_find_list () {
+	local file_path="$1"
+	cat $file_path  | while IFS='' read -r line; do
+		trim_line=$(echo $line) # trim
+
+		## https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html
+		## ignore leading #
+		if [ "${trim_line:0:1}" == '#' ]; then
+			continue;
+		fi
+
+		## ignore empty line
+		if [[ -z "$trim_line" ]]; then
+			continue;
+		fi
+
+		echo "$line"
+	done
+}
+
+main_package_find_list () {
+	util_package_find_list "package-list.txt"
+}
+
+main_package_find_list_raw () {
+	cat "package-list.txt"
+}
+##
+### Tail: util_package
+################################################################################
+
+
+################################################################################
+### Head: main
+##
+main_package_install () {
+	# sudo pacman -Sy --needed $(cat package-list.txt)
+	# sudo pacman -Sy --needed $(main_package_find_list_raw)
+	sudo pacman -Sy --needed $(main_package_find_list)
+	# pamac install $(main_package_find_list)
+	# pamac build $(main_package_find_list)
+	#yay -Sy --needed $(main_package_find_list)
+}
+
+main_package_install
+##
+### Tail: main
+################################################################################
