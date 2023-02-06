4f15c1774e9273b35f95ece591ebccdf33fd783a
samwhelp <samwhelp@users.noreply.github.com>
2023-02-07 07:08:16 +0800

    fix

--- _demo/adjustment/tool/thunar/config/thunar/Thunar/accels.scm
+++ _demo/adjustment/tool/thunar/config/thunar/Thunar/accels.scm
@@ -1,3 +1,2 @@
-(gtk_accel_path "<Actions>/ThunarActions/uca-action-1367866030392833-1" "F4")
-(gtk_accel_path "<Actions>/ThunarActions/uca-action-1367866030392833-2" "F3")
-(gtk_accel_path "<Actions>/ThunarActions/uca-action-1367866030392883-2" "F2")
+(gtk_accel_path "<Actions>/ThunarActions/uca-action-Action_Terminal-010" "F4")
+(gtk_accel_path "<Actions>/ThunarActions/uca-action-Action_Text_Editor-010" "F2")
