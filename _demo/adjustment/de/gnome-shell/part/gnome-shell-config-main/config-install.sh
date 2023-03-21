9070d1a7db1ce1f0b8b62799a03e48405694dc90
samwhelp <samwhelp@users.noreply.github.com>
2023-03-21 20:07:00 +0800

    fix

--- _demo/adjustment/de/gnome-shell/part/gnome-shell-config-main/config-install.sh
+++ _demo/adjustment/de/gnome-shell/part/gnome-shell-config-main/config-install.sh
@@ -30,10 +30,41 @@ gnome_shell_config_install () {
 
 gnome_shell_config_main () {
 
+	gnome_shell_config_main_config_wm
+
+	gnome_shell_config_main_config_keyboard
+
+	gnome_shell_config_main_config_sound
+
+}
+
+gnome_shell_config_main_config_wm () {
+
+
+	##
+	## /usr/share/glib-2.0/schemas/org.gnome.desktop.wm.preferences.gschema.xml
+	##
+
+
+	##
+	## gsettings list-recursively | grep org.gnome.desktop.wm.preferences
+	## gsettings list-recursively org.gnome.desktop.wm.preferences
+	## dconf dump /org/gnome/desktop/wm/preferences/
+	## dconf dump / | grep 'org/gnome/desktop/wm/preferences' -A 10
+	##
+
+
+	echo
+	echo "gsettings set org.gnome.desktop.wm.preferences action-double-click-titlebar 'toggle-maximize'"
+	gsettings set org.gnome.desktop.wm.preferences action-double-click-titlebar "'toggle-maximize'"
 
 	echo
 	echo "gsettings set org.gnome.desktop.wm.preferences action-middle-click-titlebar 'toggle-shade'"
 	gsettings set org.gnome.desktop.wm.preferences action-middle-click-titlebar "'toggle-shade'"
+
+	echo
+	echo "gsettings set org.gnome.desktop.wm.preferences action-right-click-titlebar 'menu'"
+	gsettings set org.gnome.desktop.wm.preferences action-right-click-titlebar "'menu'"
 
 
 	echo
@@ -58,8 +89,77 @@ gnome_shell_config_main () {
 	echo "gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true"
 	gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
 
+	echo
+	echo "gsettings set org.gnome.desktop.wm.preferences raise-on-click true"
+	gsettings set org.gnome.desktop.wm.preferences raise-on-click true
+
 
 	echo
+	echo "gsettings set org.gnome.desktop.wm.preferences focus-mode 'click'"
+	gsettings set org.gnome.desktop.wm.preferences focus-mode "'click'"
+
+	echo
+	echo "gsettings set org.gnome.desktop.wm.preferences focus-new-windows 'smart'"
+	gsettings set org.gnome.desktop.wm.preferences focus-new-windows "'smart'"
+
+	echo
+}
+
+gnome_shell_config_main_config_keyboard () {
+
+
+
+	##
+	## /usr/share/glib-2.0/schemas/org.gnome.desktop.peripherals.gschema.xml
+	##
+
+
+	##
+	## gsettings list-recursively | grep org.gnome.desktop.peripherals
+	## gsettings list-recursively org.gnome.desktop.peripherals.keyboard
+	## dconf dump /org/gnome/desktop/peripherals/keyboard/
+	## dconf dump / | grep 'org/gnome/desktop/peripherals/keyboard' -A 10
+	##
+
+
+	echo
+	echo "gsettings set org.gnome.desktop.peripherals.keyboard numlock-state false"
+	gsettings set org.gnome.desktop.peripherals.keyboard numlock-state false
+
+	echo
+	echo "gsettings set org.gnome.desktop.peripherals.keyboard remember-numlock-state true"
+	gsettings set org.gnome.desktop.peripherals.keyboard remember-numlock-state true
+
+
+
+	echo
+
+}
+
+gnome_shell_config_main_config_sound () {
+
+
+
+	##
+	## /usr/share/glib-2.0/schemas/org.gnome.desktop.sound.gschema.xml
+	##
+
+
+	##
+	## gsettings list-recursively | grep org.gnome.desktop.sound
+	## gsettings list-recursively org.gnome.desktop.sound
+	## dconf dump /org/gnome/desktop/sound/
+	## dconf dump / | grep 'org/gnome/desktop/sound' -A 10
+	##
+
+
+	echo
+	echo "gsettings set org.gnome.desktop.sound event-sounds false"
+	gsettings set org.gnome.desktop.sound event-sounds false
+
+
+	echo
+
 }
 
 ##
