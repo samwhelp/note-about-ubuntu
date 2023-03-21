efe70d0e5721751a175753d179335f0d0160dcc2
samwhelp <samwhelp@users.noreply.github.com>
2023-03-21 11:53:51 +0800

    fix

--- _demo/adjustment/de/cinnamon/part/cinnamon-config-main/config-install.sh
+++ _demo/adjustment/de/cinnamon/part/cinnamon-config-main/config-install.sh
@@ -42,6 +42,8 @@ cinnamon_config_main_config () {
 
 	cinnamon_config_main_config_keyboard
 
+	cinnamon_config_main_config_sound
+
 	cinnamon_config_main_config_applets
 
 }
@@ -55,6 +57,7 @@ cinnamon_config_main_config_wm () {
 
 
 	##
+	## gsettings list-recursively | grep org.cinnamon.desktop.wm.preferences
 	## gsettings list-recursively org.cinnamon.desktop.wm.preferences
 	## dconf dump /org/cinnamon/desktop/wm/preferences/
 	## dconf dump / | grep 'org/cinnamon/desktop/wm/preferences' -A 10
@@ -124,7 +127,7 @@ cinnamon_config_main_config_keyboard () {
 
 
 	##
-	## /usr/share/glib-2.0/schemas/org.cinnamon.desktop.wm.preferences.gschema.xml
+	## /usr/share/glib-2.0/schemas/org.cinnamon.desktop.peripherals.gschema.xml
 	##
 
 
@@ -150,23 +153,65 @@ cinnamon_config_main_config_keyboard () {
 
 }
 
+cinnamon_config_main_config_sound () {
+
+
+
+	##
+	## /usr/share/glib-2.0/schemas/org.cinnamon.desktop.sound.gschema.xml
+	##
+
+
+	##
+	## gsettings list-recursively | grep org.cinnamon.desktop.sound
+	## gsettings list-recursively org.cinnamon.desktop.sound
+	## dconf dump /org/cinnamon/desktop/sound/
+	## dconf dump / | grep 'org/cinnamon/desktop/sound' -A 10
+	##
+
+
+	echo
+	echo "gsettings set org.cinnamon.desktop.sound event-sounds false"
+	gsettings set org.cinnamon.desktop.sound event-sounds false
+
+
+	echo
+
+}
+
 cinnamon_config_main_config_applets () {
 
 
+	##
+	## /usr/share/glib-2.0/schemas/org.cinnamon.desktop.sound.gschema.xml
+	##
 
+
+	##
+	## gsettings list-recursively | grep 'org.cinnamon '
+	## gsettings list-recursively org.cinnamon
+	## dconf dump /org/cinnamon/
+	## dconf dump / | grep '^\[org/cinnamon\]' -A 10
+	##
+
+	echo
 	echo 'gsettings set org.cinnamon enabled-applets "'"['panel1:left:0:menu@cinnamon.org:0', 'panel1:left:1:show-desktop@cinnamon.org:1', 'panel1:left:2:grouped-window-list@cinnamon.org:2', 'panel1:right:1:systray@cinnamon.org:3', 'panel1:right:2:xapp-status@cinnamon.org:4', 'panel1:right:3:notifications@cinnamon.org:5', 'panel1:right:4:printers@cinnamon.org:6', 'panel1:right:5:removable-drives@cinnamon.org:7', 'panel1:right:6:keyboard@cinnamon.org:8', 'panel1:right:7:favorites@cinnamon.org:9', 'panel1:right:8:network@cinnamon.org:10', 'panel1:right:9:sound@cinnamon.org:11', 'panel1:right:10:power@cinnamon.org:12', 'panel1:center:0:calendar@cinnamon.org:13', 'panel1:right:0:workspace-switcher@cinnamon.org:14']"'"'
 	gsettings set org.cinnamon enabled-applets "['panel1:left:0:menu@cinnamon.org:0', 'panel1:left:1:show-desktop@cinnamon.org:1', 'panel1:left:2:grouped-window-list@cinnamon.org:2', 'panel1:right:1:systray@cinnamon.org:3', 'panel1:right:2:xapp-status@cinnamon.org:4', 'panel1:right:3:notifications@cinnamon.org:5', 'panel1:right:4:printers@cinnamon.org:6', 'panel1:right:5:removable-drives@cinnamon.org:7', 'panel1:right:6:keyboard@cinnamon.org:8', 'panel1:right:7:favorites@cinnamon.org:9', 'panel1:right:8:network@cinnamon.org:10', 'panel1:right:9:sound@cinnamon.org:11', 'panel1:right:10:power@cinnamon.org:12', 'panel1:center:0:calendar@cinnamon.org:13', 'panel1:right:0:workspace-switcher@cinnamon.org:14']"
 
+	echo
 	echo "gsettings set org.cinnamon next-applet-id 15"
 	gsettings set org.cinnamon next-applet-id 15
 
+	echo
 	echo 'gsettings set org.cinnamon panels-enabled "'"['1:0:top']"'"'
 	gsettings set org.cinnamon panels-enabled "['1:0:top']"
 
+	echo
 	echo "gsettings set org.cinnamon panel-edit-mode false"
 	gsettings set org.cinnamon panel-edit-mode false
 
 
+	echo
 
 }
 
@@ -196,14 +241,20 @@ cinnamon_config_misc_config () {
 
 cinnamon_config_misc_config_rc () {
 
+	echo
 	echo "rm -rf $HOME/.cinnamon"
 	rm -rf "$HOME/.cinnamon"
 
+	echo
 	echo "mkdir -p $HOME/.cinnamon"
 	mkdir -p "$HOME/.cinnamon"
 
+	echo
 	echo "cp -af config/cinnamon/config/.cinnamon/. $HOME/.cinnamon"
 	cp -af "config/cinnamon/config/.cinnamon/." "$HOME/.cinnamon"
+
+
+	echo
 
 }
 
