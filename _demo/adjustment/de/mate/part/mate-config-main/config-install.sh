76a2cc2f299d33af8e9248116f20a71c3c85b04c
samwhelp <samwhelp@users.noreply.github.com>
2023-03-21 20:30:54 +0800

    fix

--- _demo/adjustment/de/mate/part/mate-config-main/config-install.sh
+++ _demo/adjustment/de/mate/part/mate-config-main/config-install.sh
@@ -8,10 +8,6 @@ set -e
 ##
 mate_config_install () {
 
-	echo
-	echo "## Config: mate"
-	echo
-
 	mate_config_main
 
 	echo
@@ -29,10 +25,39 @@ mate_config_install () {
 ##
 
 mate_config_main () {
-	mate_config_main_wm
+
+	echo
+	echo "##"
+	echo "## Config: mate_config_main"
+	echo "##"
+	echo
+
+	mate_config_main_config_wm
+
+	mate_config_main_config_keyboard
+
+	mate_config_main_config_sound
+
 }
 
-mate_config_main_wm () {
+mate_config_main_config_wm () {
+
+
+	##
+	## /usr/share/glib-2.0/schemas/org.mate.Marco.general.gschema.xml
+	##
+
+
+	##
+	## gsettings list-recursively | grep org.mate.Marco.general
+	## gsettings list-recursively org.mate.Marco.general
+	## dconf dump /org/mate/marco/general/
+	## dconf dump / | grep 'org/mate/marco/general' -A 10
+	##
+
+
+	echo "## mate_config_main_config_wm"
+
 
 	echo
 	echo "gsettings set org.mate.Marco.general action-double-click-titlebar 'toggle_maximize'"
@@ -66,6 +91,69 @@ mate_config_main_wm () {
 
 
 	echo
+}
+
+mate_config_main_config_keyboard () {
+
+
+
+	##
+	## /usr/share/glib-2.0/schemas/org.mate.desktop.peripherals.gschema.xml
+	##
+
+
+	##
+	## gsettings list-recursively | grep org.mate.peripherals
+	## gsettings list-recursively org.mate.peripherals-keyboard
+	## dconf dump /org/mate/desktop/peripherals-keyboard/
+	## dconf dump / | grep 'org/mate/desktop/peripherals-keyboard' -A 10
+	##
+
+
+	echo "## mate_config_main_config_keyboard"
+
+
+	echo
+	echo "gsettings set org.mate.peripherals-keyboard numlock-state 'off'"
+	gsettings set org.mate.peripherals-keyboard numlock-state "'off'"
+
+	echo
+	echo "gsettings set org.mate.peripherals-keyboard remember-numlock-state true"
+	gsettings set org.mate.peripherals-keyboard remember-numlock-state true
+
+
+
+	echo
+
+}
+
+mate_config_main_config_sound () {
+
+
+
+	##
+	## /usr/share/glib-2.0/schemas/org.mate.sound.gschema.xml
+	##
+
+
+	##
+	## gsettings list-recursively | grep org.mate.sound
+	## gsettings list-recursively org.mate.sound
+	## dconf dump /org/mate/sound/
+	## dconf dump / | grep 'org/mate/sound' -A 10
+	##
+
+
+	echo "## mate_config_main_config_sound"
+
+
+	echo
+	echo "gsettings set org.mate.sound event-sounds false"
+	gsettings set org.mate.sound event-sounds false
+
+
+	echo
+
 }
 
 ##
