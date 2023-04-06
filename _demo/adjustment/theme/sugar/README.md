136b06c149fcab5178204055588670de60cf618d
samwhelp <samwhelp@users.noreply.github.com>
2023-04-07 07:30:42 +0800

    fix

---
+++ _demo/adjustment/theme/sugar/README.md
@@ -0,0 +1,20 @@
+
+
+## Search
+
+* [deepin theme](https://archlinux.org/packages/?sort=&q=deepin+theme&maintainer=&flagged=)
+
+
+## Arch Package
+
+* [deepin-gtk-theme](https://archlinux.org/packages/community/any/deepin-gtk-theme/)
+* [deepin-icon-theme](https://archlinux.org/packages/community/any/deepin-icon-theme/)
+* [deepin-sound-theme](https://archlinux.org/packages/community/any/deepin-sound-theme/)
+
+
+## Source
+
+* [deepin-gtk-theme](https://github.com/linuxdeepin/deepin-gtk-theme)
+* [deepin-icon-theme](https://github.com/linuxdeepin/deepin-icon-theme)
+* [deepin-sound-theme](https://github.com/linuxdeepin/deepin-sound-theme)
+
---
+++ _demo/adjustment/theme/sugar/package-install.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+################################################################################
+### Head: util_package
+##
+
+util_package_find_list () {
+	local file_path="$1"
+	cat $file_path  | while IFS='' read -r line; do
+		trim_line=$(echo $line) # trim
+
+		## https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html
+		## ignore leading #
+		if [ "${trim_line:0:1}" == '#' ]; then
+			continue;
+		fi
+
+		## ignore empty line
+		if [[ -z "$trim_line" ]]; then
+			continue;
+		fi
+
+		echo "$line"
+	done
+}
+
+main_package_find_list () {
+	util_package_find_list "package-list.txt"
+}
+
+main_package_find_list_raw () {
+	cat "package-list.txt"
+}
+
+##
+### Tail: util_package
+################################################################################
+
+
+################################################################################
+### Head: package_install
+##
+
+main_package_install () {
+
+	##
+	## ## Debian or Ubuntu
+	##
+
+	# sudo apt-get install $(cat package-list.txt)
+	# sudo apt-get install $(main_package_find_list_raw)
+	# sudo apt-get install $(main_package_find_list)
+
+
+	##
+	## ## Arch or Manjaro
+	##
+
+	# sudo pacman -Sy --needed $(cat package-list.txt)
+	# sudo pacman -Sy --needed $(main_package_find_list_raw)
+	# sudo pacman -Sy --needed $(main_package_find_list)
+	# pamac install $(main_package_find_list)
+	# pamac build $(main_package_find_list)
+	yay -Sy --needed $(main_package_find_list)
+
+}
+
+##
+### Tail: package_install
+################################################################################
+
+
+################################################################################
+### Head: Main
+##
+
+__main__ () {
+
+	main_package_install
+
+}
+
+##
+## Start
+##
+__main__
+
+##
+### Tail: Main
+################################################################################
